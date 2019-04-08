284e45d22c5dd03b24f01787251dd72eaf43677b
Islambek123 <topkokos1337@gmail.com>
2019-04-08 22:52:18 +0300

    Init React

---
+++ frontend/my-app/src/NavigationBar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class NavigationBar extends Component {
+    render(){
+        return(
+            <nav className="navbar navbar-default">
+                <div className="navbar-header">
+                    <h1>Hello</h1>
+                </div>
+            </nav>
+        );
+    }
+}
+
+export default NavigationBar;
